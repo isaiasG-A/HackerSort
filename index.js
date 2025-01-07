cd26945833d3223dea2a03996e8d5ca05776c7e0
isaiasG-A <isaiasgarciaariza@gmail.com>
2025-01-06 18:09:18 -0800

    code has been updated in order to extract articles based on their ID

--- index.js
+++ index.js
@@ -3,12 +3,42 @@ const { chromium } = require("playwright");
 
 async function sortHackerNewsArticles() {
   // launch browser
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true }); //headless mode is used for automation.
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
+  try {
+ // go to Hacker News
+ await page.goto("https://news.ycombinator.com/newest");
+ 
+ //Extracting articles
+ const articles = await page.$$eval(".athing", (nodes) => {
+  return nodes.map((node) => ({
+    id: parseInt(node.getAttribute("id"), 10), //ids will be used to check sorting.
+  }))
+ });
+
+ //Validation of number of articles.
+ if(articles.length !== 100) {
+  throw new Error(`Expected 100 articles, but found ${articles.length}`);
+ }
+
+ console.log("100 articles were found");
+
+ //Validation of sorting.
+const ids = articles.map((article) => article.id);
+const sortedIds = [...ids].sort((a, b) => a - b); //Sorting ids in descending order.
+
+if(JSON.stringify(ids) !== JSON.stringify(sortedIds)) {
+  throw new Error("Articles are not sorted from newest to oldest");
+}
+ console.log("Articles are sorted from newest to oldest");
+} catch (error) {
+    console.error("Validation error", error);
+} finally {
+    await browser.close();
+    return;
+  }
 }
 
 (async () => {
